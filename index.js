87f591c4fa577926935b910049c4f94c40c006c4
Aman Kumar Yadav <amankumaryadavasasmd@gmail.com>
2022-08-28 19:20:11 +0530

    login-problem-resolved

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const process = require('process')
 const app = express();
 
 app.use( express.static(path.join(__dirname,'dist')));
@@ -8,8 +8,8 @@ app.use( express.static(path.join(__dirname,'dist')));
 app.get('*', (req, res, next) => {
     res.sendFile( path.join( __dirname, 'dist/index.html'));
 })
-const PORT =  8082;
+const PORT =  process.env.PORT_NUMBER | 8081;
 
-app.listen( PORT, () => {
+app.listen( process.env.PORT | PORT, () => {
     console.log(`server started on port = ${PORT}`);
 })
