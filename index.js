27052c12026ae5bd58e0ccb4e9f478d1649e0445
Aman kumar Yadav <107494596+AmanYadavB@users.noreply.github.com>
2022-08-28 18:27:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const process = require('process');
 const app = express();
 
 app.use( express.static(path.join(__dirname,'dist')));
@@ -8,7 +8,7 @@ app.use( express.static(path.join(__dirname,'dist')));
 app.get('*', (req, res, next) => {
     res.sendFile( path.join( __dirname, 'dist/index.html'));
 })
-const PORT =  8082;
+const PORT =  process.env.PORT_NUMBER | 8081;
 
 app.listen( PORT, () => {
     console.log(`server started on port = ${PORT}`);
